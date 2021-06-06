911ddb08065cce11e151e6ac98e764fc14248877
Salman A <salmana.ar@gmail.com>
2021-06-06 22:59:46 +0530

    Completed the project!

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+import Meme from '../artifacts/Meme.json'
 import './App.css';
 
 const ipfsClient = require('ipfs-http-client')
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 
 class App extends Component {
+  async componentDidMount() {
+    await this.loadWeb3();
+    await this.loadBlockchain();
+  }
+
+  async loadBlockchain() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Meme.networks[networkId]
+    if(networkData) {
+      const contract = web3.eth.Contract(Meme.abi, networkData.address)
+      this.setState({ contract })
+      const memeHash = await contract.methods.get().call()
+      this.setState({ memeHash })
+    } else {
+      window.alert('Smart contract not deployed to detected network.')
+    }
+  }
+
   constructor(props) {
     super(props);
     this.state = {
+      account: "",
+      contract: null,
       buffer: null,
-      memeHash: 'QmRjg6pi6sbsNfqdVXDsBvStfXPsnwYBAFwsTcXVDadAe4'
+      memeHash: ''
     };
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Please install MetaMask!")
+    }
   }
 
   captureFile = (event) => {
     event.preventDefault();
-    // console.log(event.target.files);
     // Process file for IPFS
     const file = event.target.files[0];
     const reader = new window.FileReader();
@@ -26,18 +61,21 @@ class App extends Component {
   }
 
   // Sample hash: "QmRjg6pi6sbsNfqdVXDsBvStfXPsnwYBAFwsTcXVDadAe4"
-  // Sample URL: https://ipfs.infura.io/ipfs/QmRjg6pi6sbsNfqdVXDsBvStfXPsnwYBAFwsTcXVDadAe4
+  // Sample URL: https://ipfs.infura.io/ipfs/QmPAXmygc4PqrWEWeXheVVU3ntzhfefxYvk34trvPnymFt
   onSubmit = (event) => {
     event.preventDefault();
     console.log("Inside")
     ipfs.add(this.state.buffer, (error, result) => {
       console.log("IPFS result", result);
+      const memeHash = result[0].hash;
       if (error) {
         console.error(error)
         return
       }
-      this.setState({ memeHash: result[0].hash })
-      console.log(this.state.memeHash)
+      this.state.contract.methods.set(memeHash).send({ from: this.state.account })
+        .then(() => {
+          this.setState({ memeHash });
+        })
     })
   }
 
